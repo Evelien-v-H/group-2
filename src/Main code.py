10a2d1c4e54d4c1715e80e6c21e7fd650ca8c4e2
Evelien van Hengst <github@ee.vhengst.nl>
2025-12-15 07:19:52 +0100

    done

--- src/Main code.py
+++ src/Main code.py
@@ -321,30 +321,7 @@ def train_model(X,y,n_estimators=100,  criterion='squared_error', max_depth=None
     Max_samples is an integer, float or None.
     
     Output: A random forest model  """
-    #ja er komt een uitleg wat alles is en ik ga nog selecteren wat relevant is, dit zijn de standaard waarde van de makers van het model
-    #Ik kan doordat ik dit tijdens de datacombinatie gaan doen omdat ik daar errors had nu niet de X,y die daaruit komt gebruiken dus die eventuele errors zal ik nog op moeten lossen
-
-    #n_estimators is het aantal bomen dat je wilt gaan gebruiken, dit lijkt mij relevant
-    #criterion, dit is de Loss functie die je gebruikt om op zoek te gaan naar de beste boom,
-    #Max_depth De maximum depth van de boom, je kan dus ook het maximum qua aantal takken voorstellen. Dit is iets anders dan de minimum aantal samples per afsplitsing. Dit is een hyperparameter die we uit zullen moeten gaan testen
-    #min_samples_split minimaal aantal samples per split, dit is een hyperparameter die we sws moeten gaan testen
-    #min_samples_leaf, minimaal aantal samples die nodig zijn bij een leaf node, dus met hoeveel je uiteindelijk een keus maakt --> ook testen
-    #max_features, waordt er gekenen naar het maximaal aantal features die een boom gebruikt om een boom te maken --> redelijk relevant, miss ook voor featureselection
-    #bootstrap --> is relevant, moet aanstaan
-    #random_state --> kan denk ik wel nuttig zijn tijdens het testen, maar ook half
-
-
-    #oob_scire --> out of bag, kan relevant miss ook in plaats van crossvalidation
-    #max_samples --> kan relevant zijn, maar ik zou dit niet als eerste testen, als je het niet test is dat denk ik ook prima
-
-    #min_weight_fraction_leaf --> is een andere methode van het aantal uiteindelijk samples bepalen, hoeft niet uitgetest te worden
-    #max_leaf_nodes wordt gekeken naar hoeveel leafs er maximaal zijn, kan je denk ik beter met andere features doen
-    #min_impurity_decrease, de minimale verbetering --> ik denk dat dit heel lastig is, voorkomt miss overfitting, maar ik denk dat dit te veel extra is
-    #n_jobs, hij gaat dan meerdere dingen tegelijk doen, is denk ik niet heel relevant
-    #verbose, niet heel nuttig geeft je eventueel meer inzicht over hoever die is
-    #warm_start --> herbruikt dan de vorige call om beter te fitten, maar ik denk dat wij dit juist niet willen
-    #ccp_alpha --> kan gebruikt worden voor overfitten maar is denk ik nu onnodig complex
-    #monotonic_cst, je kan beperkingen aan de richting van invloed van features, is denk ik onnodig ingewikkeld
+    
     random_forest=sklearn.ensemble.RandomForestRegressor(n_estimators=n_estimators,  criterion=criterion, max_depth=max_depth, min_samples_split=min_samples_split, 
                                                          min_samples_leaf=min_samples_leaf,min_weight_fraction_leaf=min_weight_fraction_leaf, max_features=max_features, 
                                                          max_leaf_nodes=max_leaf_nodes, min_impurity_decrease=min_impurity_decrease, bootstrap=bootstrap, 
